b761295fc8f3bf701722c69eb9bd36bf99ba88d0
Brandon OToole <otoolb@gmail.com>
2023-09-24 21:13:07 -0700

    Update QuadData.js simplify data storage

--- QuadData.js
+++ QuadData.js
@@ -3,35 +3,31 @@
 export class QuadData {
     constructor(data, left, right, top, bottom) {
         this._data = data;
-
-        this._xi = left;
-        this._xf = right;
-        this._yi = top;
-        this._yf = bottom;
+        this._bounds = [left, right, top, bottom];
     }
 
     // get the data contained in this space
     get data() { return this._data }
 
     // get the horizonal, initial bounds coordinate
-    get xi() { return this._xi; }
+    get xi() { return this._bounds[0]; }
 
     // get the horizonal, final bounds coordinate
-    get xf() { return this._xf; }
+    get xf() { return this._bounds[1]; }
 
     // get the vertical, initial bounds coordinate
-    get yi() { return this._yi; }
+    get yi() { return this._bounds[2]; }
 
     // get the vertical, final bounds coordinate
-    get yf() { return this._yf; }
+    get yf() { return this._bounds[3]; }
 
     // get the width of coordinates spanned by the data (cache result)
     get width() {
-        return this._width = this._width || this._xf - this._xi;
+        return this._width = this._width || this._bounds[1] - this._bounds[0];
     }
 
     // get the height of coordinates spanned by the data (cache result)
     get height() {
-        return this._height = this._height || this._yf - this._yi;
+        return this._height = this._height || this._bounds[3] - this._bounds[2];
     }
 }
